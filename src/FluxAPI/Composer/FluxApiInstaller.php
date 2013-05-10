497e01cb274614d9908ec839745c106bb4382469
Ondrej Brinkel <info@anzui.de>
2013-05-10 09:57:26 +0200

    extending from LibraryInstaller now

--- src/FluxAPI/Composer/FluxApiInstaller.php
+++ src/FluxAPI/Composer/FluxApiInstaller.php
@@ -2,9 +2,27 @@
 
 namespace FluxAPI\Composer;
 
-class FluxApiInstaller extends \Composer\Installers\BaseInstaller
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class FluxApiInstaller extends LibraryInstaller
 {
-    protected $locations = array(
-        'plugin'    => 'Plugins/{$name}/',
+    protected $_locations = array(
+        'flux-api-plugin'    => 'Plugins/',
     );
+
+    public function getInstallPath(PackageInterface $package)
+    {
+        $type = $package->getType();
+        if (isset($this->_locations[$type])) {
+            return $this->_locations[$type] . $package->getPrettyName();
+        } else {
+            throw new \InvalidArgumentException('Unable to install package, it is not within the supported package types.');
+        }
+    }
+
+    public function supports($packageType)
+    {
+        return in_array($packageType, array_keys($this->_locations));
+    }
 }
